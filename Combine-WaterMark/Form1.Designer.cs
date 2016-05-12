6830ef2d2930f0a13994040c162f6345e80df47f
litengfei-ff <asdasd1ZX>
2016-05-12 19:25:47 +0800

    'first'

---
+++ Combine-WaterMark/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Combine_WaterMark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Do = new System.Windows.Forms.Button();
+            this.NeedWater = new System.Windows.Forms.Button();
+            this.WaterLabel = new System.Windows.Forms.Label();
+            this.SelectWater = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.yulan = new System.Windows.Forms.PictureBox();
+            this.show = new System.Windows.Forms.Button();
+            this.up = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.selectWaterDialog = new System.Windows.Forms.OpenFileDialog();
+            this.NeedWaterDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.yulan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(951, 545);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AutoScroll = true;
+            this.tabPage1.Controls.Add(this.Do);
+            this.tabPage1.Controls.Add(this.NeedWater);
+            this.tabPage1.Controls.Add(this.WaterLabel);
+            this.tabPage1.Controls.Add(this.SelectWater);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(943, 519);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "添加水印";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Do
+            // 
+            this.Do.Location = new System.Drawing.Point(232, 64);
+            this.Do.Name = "Do";
+            this.Do.Size = new System.Drawing.Size(75, 23);
+            this.Do.TabIndex = 3;
+            this.Do.Text = "添加水印";
+            this.Do.UseVisualStyleBackColor = true;
+            this.Do.Click += new System.EventHandler(this.Do_Click);
+            // 
+            // NeedWater
+            // 
+            this.NeedWater.Location = new System.Drawing.Point(18, 64);
+            this.NeedWater.Name = "NeedWater";
+            this.NeedWater.Size = new System.Drawing.Size(152, 23);
+            this.NeedWater.TabIndex = 2;
+            this.NeedWater.Text = "选择需要添加水印文件";
+            this.NeedWater.UseVisualStyleBackColor = true;
+            this.NeedWater.Click += new System.EventHandler(this.NeedWater_Click);
+            // 
+            // WaterLabel
+            // 
+            this.WaterLabel.AutoSize = true;
+            this.WaterLabel.Location = new System.Drawing.Point(136, 27);
+            this.WaterLabel.Name = "WaterLabel";
+            this.WaterLabel.Size = new System.Drawing.Size(0, 12);
+            this.WaterLabel.TabIndex = 1;
+            // 
+            // SelectWater
+            // 
+            this.SelectWater.Location = new System.Drawing.Point(18, 17);
+            this.SelectWater.Name = "SelectWater";
+            this.SelectWater.Size = new System.Drawing.Size(87, 23);
+            this.SelectWater.TabIndex = 0;
+            this.SelectWater.Text = "选择水印文件";
+            this.SelectWater.UseVisualStyleBackColor = true;
+            this.SelectWater.Click += new System.EventHandler(this.SelectWater_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AutoScroll = true;
+            this.tabPage2.Controls.Add(this.yulan);
+            this.tabPage2.Controls.Add(this.show);
+            this.tabPage2.Controls.Add(this.up);
+            this.tabPage2.Controls.Add(this.down);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(943, 519);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "合并图片";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // yulan
+            // 
+            this.yulan.Location = new System.Drawing.Point(484, 61);
+            this.yulan.Name = "yulan";
+            this.yulan.Size = new System.Drawing.Size(430, 508);
+            this.yulan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yulan.TabIndex = 6;
+            this.yulan.TabStop = false;
+            this.yulan.Visible = false;
+            this.yulan.DoubleClick += new System.EventHandler(this.yulan_DoubleClick);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(484, 19);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 5;
+            this.show.Tag = ".BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;";
+            this.show.Text = "预览";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Visible = false;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // up
+            // 
+            this.up.Location = new System.Drawing.Point(350, 19);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(75, 23);
+            this.up.TabIndex = 4;
+            this.up.Tag = ".BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;";
+            this.up.Text = "上移";
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Visible = false;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // down
+            // 
+            this.down.Location = new System.Drawing.Point(269, 19);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(75, 23);
+            this.down.TabIndex = 3;
+            this.down.Tag = ".BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;";
+            this.down.Text = "下移";
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Visible = false;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "共选择 0 个图片";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "查看";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "图片文件|*.jpg;*.png;*.bmp";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "图片文件|*.jpg";
+            // 
+            // selectWaterDialog
+            // 
+            this.selectWaterDialog.Filter = "图片文件|*.jpg;*.png;*.bmp";
+            // 
+            // NeedWaterDialog
+            // 
+            this.NeedWaterDialog.Filter = "图片文件|*.jpg;*.png;*.bmp";
+            this.NeedWaterDialog.Multiselect = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 579);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "ImageCombine-WaterMark";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.yulan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button down;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.PictureBox yulan;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button NeedWater;
+        private System.Windows.Forms.Label WaterLabel;
+        private System.Windows.Forms.Button SelectWater;
+        private System.Windows.Forms.Button Do;
+        private System.Windows.Forms.OpenFileDialog selectWaterDialog;
+        private System.Windows.Forms.OpenFileDialog NeedWaterDialog;
+    }
+}
+
---
+++ Combine-WaterMark/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Combine_WaterMark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //定义存放文件名和路径的泛型SoredList
+        SortedList<int,string> list=new SortedList<int,string>();
+
+        //选择文件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //点击打开按钮而且选择文件不为空时执行
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileNames.Length != 0)
+            {
+                Console.WriteLine("选择文件完毕");
+                label1.Text = "共选择 "+openFileDialog1.FileNames.Length+" 个图片";
+
+            }
+        }
+        //查看文件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //隐藏预览
+            yulan.Visible = false;
+            //清空list
+            list.Clear();
+            //清空预览图片
+            yulan.Image = null;
+            //显示移动、预览按钮
+            down.Visible = up.Visible = show.Visible = false;
+            if (openFileDialog1.FileNames.Length>1)
+            {
+                  down.Visible = up.Visible = show.Visible = true;
+            }
+
+            //循环添加list
+            for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+            {
+                list.Add(i, openFileDialog1.FileNames[i]);
+                
+            }
+
+            MBind();
+          
+        }
+        //绑定方法
+        private void MBind()
+        {
+            //清除Panel
+            for (int i = 0; i < tabPage2.Controls.Count; i++)
+            {
+                if (tabPage2.Controls["sp" + i] != null)
+                {
+                    tabPage2.Controls["sp" + i].Dispose();
+                }
+            }
+
+           
+            //定义初始位置
+            Point position = new Point(20, 90);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+
+            //动态生产 panel,label picbox
+            string FullName = list[i];
+            string Name = list[i].Substring(list[i].LastIndexOf("\\")+1);
+
+            //Label
+            Label slabel = new Label();
+            slabel.AutoSize = true;
+            slabel.Location = new System.Drawing.Point(13, 13);
+            slabel.Name = "slabel1";
+            slabel.Size = new System.Drawing.Size(40, 12);
+            slabel.TabIndex = 0;
+            slabel.Text = "文件名：" + Name;
+
+
+            //pic
+            //获取选择图片对象
+            Image SrcImg = Image.FromFile(FullName);
+            //新建指定高宽的图片
+            Bitmap newImg = new System.Drawing.Bitmap(234, 113);
+            Graphics g = Graphics.FromImage(newImg);
+            g.DrawImage(SrcImg, 0, 0, 234, 113);
+
+            PictureBox spic = new PictureBox();
+            spic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            spic.Location = new System.Drawing.Point(15, 37);
+            spic.Name = "spic1";
+            spic.Size = new System.Drawing.Size(234, 113);
+            spic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            spic.TabIndex = 1;
+            spic.TabStop = false;
+            spic.Image = newImg;
+
+            //panel
+            Panel sp = new Panel();
+            sp.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            sp.Controls.Add(spic);
+            sp.Controls.Add(slabel);
+            sp.Location = position;
+            sp.Name = "sp" + i;
+            sp.Size = new System.Drawing.Size(266, 160);
+            sp.TabIndex = 3;
+            sp.Click += new EventHandler(sp_Click);
+
+
+            //position改变
+            position = new Point(position.X, position.Y + 170);
+            //添加到list
+            tabPage2.Controls.Add(sp);
+         
+            }
+        }
+        //鼠标点击显示选中
+        void sp_Click(object sender, EventArgs e)
+        {
+            for (int i=7; i < tabPage2.Controls.Count; i++)
+            {
+          
+                    (tabPage2.Controls[i] as Panel).BorderStyle = System.Windows.Forms.BorderStyle.None;
+             
+            }
+            (sender as Panel).BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+        }
+        //下移
+        private void down_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < list.Count-1; i++)
+            {
+                if ((tabPage2.Controls["sp" + i] as Panel).BorderStyle == System.Windows.Forms.BorderStyle.Fixed3D) {
+
+                    string temp;
+                    temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    MBind();
+                }
+            }
+        }
+        //上移
+        private void up_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                if ((tabPage2.Controls["sp" + i] as Panel).BorderStyle == System.Windows.Forms.BorderStyle.Fixed3D)
+                {
+
+                    string temp;
+                    temp = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = temp;
+                    MBind();
+                }
+            }
+
+        }
+        //窗口改变事件
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //窗口大小改变
+            Size FormSize = this.Size;
+            tabControl1.Size = new Size((FormSize.Width - 30), FormSize.Height - 60);
+            yulan.Size = new Size((FormSize.Width - 30)/2+100, (FormSize.Height - 60)/2+100);
+        }
+        //预览
+        private void show_Click(object sender, EventArgs e)
+        {
+            yulan.Image = DrawImg();
+            yulan.Visible = true;
+          
+        }
+        //绘图方法
+        private Bitmap DrawImg()
+        {
+            int width = Image.FromFile(list[0]).Width;
+            int height = Image.FromFile(list[0]).Height;
+
+            Bitmap newImg = new System.Drawing.Bitmap(width, height * list.Count);
+            Graphics g = Graphics.FromImage(newImg);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Image SrcImg = Image.FromFile(list[i]);
+                g.DrawImage(SrcImg, 0, i * height, width, height);
+            }
+            return newImg;
+        }
+        //保存图片方法
+        private void yulan_DoubleClick(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                Bitmap newImg = DrawImg();
+                newImg.Save(saveFileDialog1.FileName,System.Drawing.Imaging.ImageFormat.Jpeg);
+             
+            }
+        }
+        //选择水印文件
+        private void SelectWater_Click(object sender, EventArgs e)
+        {
+            if (selectWaterDialog.ShowDialog() == DialogResult.OK) {
+
+                WaterLabel.Text = selectWaterDialog.SafeFileName;
+            
+            }
+        }
+        //选择需要添加水印文件
+        private void NeedWater_Click(object sender, EventArgs e)
+        {
+            if (NeedWaterDialog.ShowDialog() == DialogResult.OK)
+            {
+                //清除原label
+                int count=tabPage1.Controls.Count;
+                for (int i = 0; i <count ; i++)
+                {
+                    if (tabPage1.Controls["slabel" + i] != null)
+                    {
+                        (tabPage1.Controls["slabel" + i] as Label).Dispose();
+                    }
+                }
+
+
+                //初始化位置
+                Point position = new Point(18, 110);    
+                //生成label
+                for (int i = 0; i < NeedWaterDialog.FileNames.Length; i++)
+                {
+                        Label slabel = new Label();
+                        slabel.AutoSize = true;
+                        slabel.Location = position;
+                        slabel.Name = "slabel"+i;
+                        slabel.Size = new System.Drawing.Size(40, 12);
+                        slabel.TabIndex = 0;
+                        slabel.Text = NeedWaterDialog.SafeFileNames[i];
+                        tabPage1.Controls.Add(slabel);
+
+                        position = new Point(18,position.Y+30);
+                }
+
+            }
+        }
+
+        //点击添加水印按钮
+        private void Do_Click(object sender, EventArgs e)
+        {
+           
+            if (WaterLabel.Text!=""&&tabPage1.Controls["slabel0"]!=null)
+            {
+                //创建目录
+                if (!Directory.Exists("e:\\1\\"))
+                {
+                    Directory.CreateDirectory("e:\\1\\");
+                }
+
+                ThreadPool.QueueUserWorkItem(AddWater);
+                (sender as Button).Text = "进行中。。。";
+                (sender as Button).Enabled = false;
+            }
+
+         
+        }
+
+        //添加水印方法
+        private  void AddWater(object o)
+        {
+
+
+            for (int i = 0; i < tabPage1.Controls.Count - 4; i++)
+            {
+                string src=NeedWaterDialog.FileNames[i];
+                string water = selectWaterDialog.FileName;
+                Image SrcImg = Image.FromFile(src);
+                Image Water=Image.FromFile(water);
+
+                Graphics g = Graphics.FromImage(SrcImg);
+
+                g.DrawImage(Water, 0,0,Water.Width,Water.Height );
+                SrcImg.Save("E:\\1\\water-"+src.Substring(src.LastIndexOf("\\")+1) ,System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            }
+
+            //button文字返回为原值
+            Button btn = tabPage1.Controls["Do"] as Button;
+            
+            //跨线程操作
+            this.Invoke(new Action(() => { 
+                btn.Text = "添加水印";
+                btn.Enabled = true;
+            }));
+            MessageBox.Show("ok，文件已保存到E盘下1文件夹中", "提示");
+        
+
+        }
+
+       
+    }
+}
